d4bb6847124dd48bf42b7ae4d93a97560fe36efe
Jan Henning Thorsen <jhthorsen@cpan.org>
2017-06-01 21:28:26 +0200

    Add documentation

--- lib/Mojolicious/Plugin/Pager.pm
+++ lib/Mojolicious/Plugin/Pager.pm
@@ -1,16 +1,18 @@
 package Mojolicious::Plugin::Pager;
 use Mojo::Base 'Mojolicious::Plugin';
 
-use constant PAGE_PARAM  => 'pager.page_param_name';
+use POSIX ();
+
+use constant PAGE_PARAM  => 'page_param_name';
 use constant WINDOW_SIZE => 'pager.window_size';
 
 our $VERSION = '0.01';
 
 sub pager_link {
   my ($c, $page, @args) = @_;
-  my $url = $c->url_with->query($c->stash(PAGE_PARAM) => $page->{n} || 1);
+  my $url = $c->url_with;
   my @text = ref @args eq 'CODE' ? () : ($page->{n});
-  my @classes;
+  my (@extra, @classes);
 
   push @classes, 'active' if $page->{current};
   push @classes, 'first'  if $page->{first};
@@ -18,8 +20,11 @@ sub pager_link {
   push @classes, 'next'   if $page->{next};
   push @classes, 'prev'   if $page->{prev};
   push @classes, 'page' unless @classes;
+  push @extra, rel => 'next' if $page->{next};
+  push @extra, rel => 'prev' if $page->{prev};
 
-  return $c->link_to(@text, $url, class => join(' ', @classes), @args);
+  $url->query->param($c->stash(PAGE_PARAM) => $page->{n} || 1);
+  return $c->link_to(@text, $url, class => join(' ', @classes), @extra, @args);
 }
 
 sub pages_for {
@@ -28,7 +33,7 @@ sub pages_for {
   my $current_page = $args->{current} || $c->param($c->stash(PAGE_PARAM)) || 1;
   my $pager_size   = $args->{size} || 8;
   my $window_size  = ($pager_size / 2) - 1;
-  my $total_pages  = int $args->{total_pages};
+  my $total_pages  = POSIX::ceil($args->{total_pages});
   my ($start_page, @pages);
 
   if ($current_page < $window_size) {
@@ -50,13 +55,10 @@ sub pages_for {
   }
 
   return @pages unless @pages;
+  return @pages unless $total_pages > $pager_size;
 
-  if ($current_page > 1) {
-    unshift @pages, {prev => 1, n => $current_page - 1};
-  }
-  if ($current_page < $total_pages) {
-    push @pages, {next => 1, n => $current_page + 1};
-  }
+  unshift @pages, {prev => 1, n => $current_page - 1} if $current_page > 1;
+  push @pages,    {next => 1, n => $current_page + 1} if $current_page < $total_pages;
 
   return @pages;
 }
@@ -72,3 +74,127 @@ sub register {
 }
 
 1;
+
+=encoding utf8
+
+=head1 NAME
+
+Mojolicious::Plugin::Pager - Pagination plugin for Mojolicious
+
+=head1 SYNOPSIS
+
+=head2 Example lite app
+
+  use Mojolicious::Lite;
+
+  plugin "pager";
+
+  get "/" => sub {
+    my $c = shift;
+    $c->stash(total_entries => 1431, entries_per_page => 20);
+  };
+
+=head2 Example template
+
+  <ul class="pager">
+    % for my $page (pages_for $total_entries / $entries_per_page) {
+      <li><%= pager_link $page %></li>
+    % }
+  </ul>
+
+=head2 Custom template
+
+  <ul class="pager">
+    % for my $page (pages_for $total_entries / $entries_per_page) {
+      % my $url = url_with; $url->query->param(x => $page->{n});
+      <li><%= link_to "hey!", $url %></li>
+    % }
+  </ul>
+
+=head1 DESCRIPTION
+
+L<Mojolicious::Plugin::Pager> is a L<Mojolicious> plugin for creating paged
+navigation, without getting in the way. There are other plugins which ship with
+complete markup, but this is often not the markup that I<you> want.
+
+Note that this plugin is currently EXPERIMENTAL.
+
+=head1 HELPERS
+
+=head2 pager_link
+
+  $bytestream = $c->pager_link(\%page, @args);
+  $bytestream = $c->pager_link(\%page, @args, sub { int(rand 100) });
+
+Takes a C<%page> hash and creates an anchor using
+L<Mojolicious::Controller/link_to>. C<@args> is passed on, without
+modification, to C<link_to()>. The anchor generated has some classes added.
+
+See L</pages_for> for detail about C<%page>.
+
+Examples output:
+
+  <a href="?page=2" class="prev" rel="prev">12</a>
+  <a href="?page=1" class="first">1</a>
+  <a href="?page=2" class="page">2</a>
+  <a href="?page=3" class="active">3</a>
+  <a href="?page=4" class="page">4</a>
+  <a href="?page=5" class="page">5</a>
+  <a href="?page=6" class="last">6</a>
+  <a href="?page=3" class="next" rel="next">3</a>
+
+=head2 pages_for
+
+  @pages = $self->pages_for($total_pages);
+
+Returns a list of C<%page> hash-refs, that can be passed on to L</pager_link>.
+
+Example C<%page>:
+
+  {
+    n       => 2,    # page number
+    current => 1,    # if page number matches "page" query parameter
+    first   => 1,    # if this is the first page
+    last    => 1,    # if this is the last page
+    next    => 1,    # if this is last, that brings you to the next page
+    prev    => 1,    # if this is first, that brings you to the previous page
+  }
+
+=head1 METHODS
+
+=head2 register
+
+  $app->plugin("pager" => \%config);
+
+Used to register this plugin and the L</HELPERS> above. C<%config> can be:
+
+=over 4
+
+=item * param_name
+
+The query parameter that will be looked up to figure out which page you are on.
+Can also be set in L<Mojolicious::Controller/stash> on each request under the
+name "page_param_name".
+
+Default: "page"
+
+=item * window_size
+
+Used to decide how many pages to show after/before the current page.
+
+Default: 3
+
+=back
+
+=head1 AUTHOR
+
+Jan Henning Thorsen
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2016, Jan Henning Thorsen
+
+This program is free software, you can redistribute it and/or modify it under
+the terms of the Artistic License version 2.0.
+
+=cut
--- t/pager.t
+++ t/pager.t
@@ -7,7 +7,15 @@ use Mojolicious::Lite;
 plugin 'pager';
 get
   '/' => {total_entries => 1431, entries_per_page => 20},
-  'pager';
+  'default';
+
+get
+  '/custom' => {total_entries => 57, entries_per_page => 20},
+  'custom';
+
+get
+  '/small' => {total_entries => 139, entries_per_page => 20, page_param_name => 'y'},
+  'default';
 
 my $t = Test::Mojo->new;
 
@@ -18,9 +26,10 @@ $t->get_ok('/')->status_is(200)->element_exists_not('a.last')->element_exists_no
 
 $t->get_ok('/?page=3')->status_is(200)->element_exists_not('a.last')
   ->element_exists_not('a[href="/page=9"]')->element_count_is('a', 10)
-  ->element_exists('a[href="/?page=2"].prev')->element_exists('a[href="/?page=1"].first')
-  ->element_exists('a[href="/?page=3"].active')->element_exists('a[href="/?page=8"].page')
-  ->element_exists('a[href="/?page=4"].next');
+  ->element_exists('a[href="/?page=2"][rel="prev"].prev')
+  ->element_exists('a[href="/?page=1"].first')->element_exists('a[href="/?page=3"].active')
+  ->element_exists('a[href="/?page=8"].page')
+  ->element_exists('a[href="/?page=4"][rel="next"].next');
 
 $t->get_ok('/?page=4')->status_is(200)->element_exists_not('a.last')->element_exists_not('a.first')
   ->element_exists_not('a[href="/page=10"]')->element_count_is('a', 10)
@@ -39,22 +48,38 @@ $t->get_ok('/?page=23')->status_is(200)->element_exists_not('a.last')
   ->element_exists('a[href="/?page=23"].active')->element_exists('a[href="/?page=28"].page')
   ->element_exists('a[href="/?page=24"].next');
 
-$t->get_ok('/?page=71')->status_is(200)->element_exists_not('a.first')
-  ->element_exists_not('a.next')->element_exists_not('a[href="/page=72"]')
-  ->element_count_is('a', 9)->element_exists('a[href="/?page=69"].page')
-  ->element_exists('a[href="/?page=70"].prev')->element_exists('a[href="/?page=71"].active.last');
+$t->get_ok('/?x=y&page=72')->status_is(200)->element_exists_not('a.first')
+  ->element_exists_not('a.next')->element_exists_not('a[href="/x=y&page=73"]')
+  ->element_count_is('a', 9)->element_exists('a[href="/?x=y&page=70"].page')
+  ->element_exists('a[href="/?x=y&page=71"].prev')
+  ->element_exists('a[href="/?x=y&page=72"].active.last');
 
-for my $p (63 .. 70) {
+for my $p (64 .. 71) {
   $t->get_ok("/?page=$p")->status_is(200)->element_exists('a.next')->element_count_is('a', 10)
     ->element_exists(qq(a[href="/?page=$p"].active));
 }
 
+$t->get_ok('/custom')->status_is(200)->element_count_is('a', 3)->text_is('a', 'hey!')
+  ->element_exists('a[href="/custom?x=3"]');
+
+$t->get_ok('/small?y=2')->status_is(200)->element_exists_not('a.prev')
+  ->element_exists_not('a.next')->element_count_is('a', 7)
+  ->element_exists('a[href="/small?y=1"].first')->element_exists('a[href="/small?y=2"].active')
+  ->element_exists('a[href="/small?y=7"].last');
+
 done_testing;
 
 __DATA__
-@@ pager.html.ep
+@@ default.html.ep
 <ul class="pager">
   % for my $page (pages_for $total_entries / $entries_per_page) {
     <li><%= pager_link $page %></li>
+  % }
+</ul>
+@@ custom.html.ep
+<ul class="pager">
+  % for my $page (pages_for $total_entries / $entries_per_page) {
+    % my $url = url_with; $url->query->param(x => $page->{n});
+    <li><%= link_to "hey!", $url %></li>
   % }
 </ul>
